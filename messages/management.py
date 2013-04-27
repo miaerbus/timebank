0ea2a51f49c24bb1494039d7ac936c8b95f1b360
Mia Erbus <mia.erbus@gmail.com>
2013-04-27 18:31:34 +0200

    prevodi

--- messages/management.py
+++ messages/management.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db.models import get_models, signals
 from django.conf import settings
 from django.utils.translation import ugettext_noop as _
@@ -6,12 +7,12 @@ if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
 
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("messages_received", _("Message Received"), _("you have received a message"), default=2)
-        notification.create_notice_type("messages_sent", _("Message Sent"), _("you have sent a message"), default=1)
-        notification.create_notice_type("messages_replied", _("Message Replied"), _("you have replied to a message"), default=1)
-        notification.create_notice_type("messages_reply_received", _("Reply Received"), _("you have received a reply to a message"), default=2)
-        notification.create_notice_type("messages_deleted", _("Message Deleted"), _("you have deleted a message"), default=1)
-        notification.create_notice_type("messages_recovered", _("Message Recovered"), _("you have undeleted a message"), default=1)
+        notification.create_notice_type("messages_received", _(u"Prejeto sporočilo"), _(u"prejeli ste sporočilo"), default=2)
+        notification.create_notice_type("messages_sent", _(u"Sporočilo poslano"), _(u"poslali ste sporočilo"), default=1)
+        notification.create_notice_type("messages_replied", _(u"Sporočilo odgovorjeno"), _(u"odgvorili ste na sporočilo"), default=1)
+        notification.create_notice_type("messages_reply_received", _(u"Odgovor sprejet"), _(u"prejeli ste odgovor na sporočilo"), default=2)
+        notification.create_notice_type("messages_deleted", _(u"Sporočilo izbrisano"), _(u"izbrisali ste sporočilo"), default=1)
+        notification.create_notice_type("messages_recovered", _(u"Sporočilo obnovljeno"), _(u"obnovili ste sporočilo"), default=1)
 
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 else:
